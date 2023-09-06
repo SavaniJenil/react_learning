704d5f759a838d8fe3d62e5b4fa810953ed754fc
jenil Savani <Savanijenil.Nitinbhai@Solera.com>
2023-09-06 19:34:03 +0530

    Completed Lecture 7

--- food_app/src/components/Header.js
+++ food_app/src/components/Header.js
@@ -1,26 +1,38 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
+import { useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
 
- const [btnName,setBtnName] = useState("Login");
+  useEffect(() => {
+    console.log("useEffect called");
+  }, [btnName]);
 
-    return (<div className="header">
+  return (
+    <div className="header">
       <div className="logo-container">
-        <img className="logo" src = {LOGO_URL} />
+        <img className="logo" src={LOGO_URL} />
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About Us</li>
-          <li>Contact Us</li>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About Us</Link></li>
+          <li><Link to="/contact">Contact Us</Link></li>
           <li>Cart</li>
-          <button className="login-btn" onClick={() => {
-            btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
-          }}>{btnName}</button>
+          <button
+            className="login-btn"
+            onClick={() => {
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
         </ul>
       </div>
-    </div>)
-  }
+    </div>
+  );
+};
 
-  export default Header;
+export default Header;
